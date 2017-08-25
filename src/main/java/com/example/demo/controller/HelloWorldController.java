94119b53928ba4ae815d1635dfc8afb8bafc74a8
韩学达 <hanxueda@qq.com>
2017-08-25 11:08:48 +0800

    demo

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -3,11 +3,21 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.User;
+
 @RestController
 public class HelloWorldController {
 
 	@RequestMapping("/hello")
     public String index() {
         return "Hello World";
+    }
+	
+	@RequestMapping("/getUser")
+    public User getUser() {
+    	User user=new User();
+    	user.setUserName("小明");
+    	user.setPassWord("123456");
+        return user;
     }
 }
